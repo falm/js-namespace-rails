d1097cda982143735ce00262f811960a38ef4628
Jason Hou <houjunjie@atyun.net>
2016-02-16 21:44:17 +0800

    ignore NameError: undefined local variable or method `controller_path'`

--- js-namespace-rails.gemspec
+++ js-namespace-rails.gemspec
@@ -18,7 +18,7 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ['lib']
 
-  s.licenses = ["MIT"]
+  spec.licenses = ["MIT"]
   # if spec.respond_to?(:metadata)
   #   spec.metadata['allowed_push_host'] = "TODO: Set to 'http://mygemserver.com' to prevent pushes to rubygems.org, or delete to allow pushes to any server."
   # end
--- lib/js_namespace_rails/action_view/helpers/asset_tag_helper.rb
+++ lib/js_namespace_rails/action_view/helpers/asset_tag_helper.rb
@@ -1,21 +1,25 @@
 
 module ActionView::Helpers::AssetTagHelper
 
-  alias_method :old_javascript_include_tag, :javascript_include_tag
+  alias_method :javascript_include_tag_without_controller, :javascript_include_tag
 
   def javascript_include_tag(*source)
 
-    origin_result = old_javascript_include_tag(*source)
+    origin_result = javascript_include_tag_without_controller(*source)
 
-    _controller = controller_path.gsub(/\//,'_')
+    if defined?(controller_path)
+      _controller = controller_path.gsub(/\//,'_')
 
-    script = <<STRING
-      document.addEventListener('DOMContentLoaded', function() {
-        document.getElementsByTagName('body')[0].setAttribute('data-controller', '#{_controller}');
-        document.getElementsByTagName('body')[0].setAttribute('data-action', '#{action_name}');
-      });
+      script = <<STRING
+        document.addEventListener('DOMContentLoaded', function() {
+          document.getElementsByTagName('body')[0].setAttribute('data-controller', '#{_controller}');
+          document.getElementsByTagName('body')[0].setAttribute('data-action', '#{action_name}');
+        });
 STRING
-    origin_result + concat(javascript_tag(script, defer: 'defer'))
+      origin_result + concat(javascript_tag(script, defer: 'defer'))
+    end
+
+    origin_result
 
   end
 
