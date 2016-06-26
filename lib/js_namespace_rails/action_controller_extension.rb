20788f21cede928fa11d6ae65cd2041193434bf7
Jason Hou <hjj1992@gmail.com>
2016-06-26 09:48:56 +0800

    syntax

--- lib/js_namespace_rails/action_controller_extension.rb
+++ lib/js_namespace_rails/action_controller_extension.rb
@@ -3,7 +3,7 @@ module JsNamespaceRails
   module ActionControllerExtension
     def js(params)
       @js_namespace_rails_params ||= {}
-      @js_namespace_rails_params.merge!(params)
+      @js_namespace_rails_params = @js_namespace_rails_params.merge(params)
     end
   end
   ::ActionController::Base.send :include, ActionControllerExtension
