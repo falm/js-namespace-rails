07756732b77bdf17031d022617454aad34dbf3d6
Jason Hou <hjj1992@gmail.com>
2016-06-30 11:51:05 +0800

    change dummy app

--- spec/dummy/app/assets/javascripts/application.js
+++ spec/dummy/app/assets/javascripts/application.js
@@ -19,9 +19,9 @@ JsSpace.on('users', {
   init: function(){
     $('.common-place').text('common_action_works')
   },
-  index: function(params){
+  index: function(){
     $('.title').text('user_list');
-    var puts = ['user_id', params.user_id, 'user_name', params.user_name].join(':');
+    var puts = ['user_id', this.params.user_id, 'user_name', this.params.user_name].join(':');
     $('.common-place').append(puts);
   },
   new: function(){
