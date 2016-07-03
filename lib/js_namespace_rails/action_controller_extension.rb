c854a990b8d952a3b7a8574c03e9cf9cb82cd84e
Jason Hou <hjj1992@gmail.com>
2016-07-03 18:13:06 +0800

    add helper method for render js

--- lib/js_namespace_rails/action_controller_extension.rb
+++ lib/js_namespace_rails/action_controller_extension.rb
@@ -1,9 +1,25 @@
 
 module JsNamespaceRails
   module ActionControllerExtension
+
+    def self.included(base)
+      base.module_eval do
+        helper_method :js_execute
+        helper_method :insert_hook_script
+      end
+    end
+
     def js(params)
       @js_namespace_rails_params ||= {}
       @js_namespace_rails_params = @js_namespace_rails_params.merge(params)
+    end
+
+    def js_execute
+      view_context.render(partial: 'js_namespace_rails/init')
+    end
+
+    def insert_hook_script
+      view_context.render(partial: 'js_namespace_rails/hook.js.erb')
     end
   end
   ::ActionController::Base.send :include, ActionControllerExtension
