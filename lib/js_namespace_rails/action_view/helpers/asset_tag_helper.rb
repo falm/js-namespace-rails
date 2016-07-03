7866d72a2128fe64d979be04763ccd4e0770aef1
Jason Hou <hjj1992@gmail.com>
2016-07-03 18:13:55 +0800

    extract script to js file

--- lib/js_namespace_rails/action_view/helpers/asset_tag_helper.rb
+++ lib/js_namespace_rails/action_view/helpers/asset_tag_helper.rb
@@ -5,23 +5,11 @@ module ActionView::Helpers::AssetTagHelper
 
   def javascript_include_tag(*source)
 
-    origin_result = javascript_include_tag_without_controller(*source)
-
     if defined?(controller_path) && !@_included
-      controller_underscore = controller_path.gsub(/\//,'_')
       @_included = true
-      script = <<STRING
-        document.addEventListener('DOMContentLoaded', function() {
-          document.getElementsByTagName('body')[0].setAttribute('data-controller', '#{controller_underscore}');
-          document.getElementsByTagName('body')[0].setAttribute('data-action', '#{action_name}');
-          window.JsSpace.params = #{@js_namespace_rails_params.to_json};
-        });
-STRING
-      origin_result + concat(javascript_tag(script, defer: 'defer'))
+      concat javascript_tag(insert_hook_script, defer: 'defer')
     end
-
-    origin_result
-
+    javascript_include_tag_without_controller(*source)
   end
 
 end
