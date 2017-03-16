2da2ff02bef53c9d720dc978a524912d1c17816c
Jason Hou <hjj1992@gmail.com>
2017-03-16 09:18:46 +0800

    move code to user.js

--- spec/dummy/app/assets/javascripts/application.js
+++ spec/dummy/app/assets/javascripts/application.js
@@ -15,20 +15,4 @@
 //= require js-namespace-rails
 //= require_tree .
 
-JsSpace.on('users', {
-  init: function(){
-    $('.common-place').text('common_action_works')
-  },
-  index: function(){
-    $('.title').text('user_list');
-    var puts = ['user_id', this.params.user_id, 'user_name', this.params.user_name].join(':');
-    $('.common-place').append(puts);
-  },
-  new: function(){
-    $('h1.title').text('new_user');
-  },
-  show: function(){
-    $('.common-place').append('show-user');
-    $('.common-place').append(this.params.message);
-  }
-});
+
