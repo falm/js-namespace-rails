62b3d06d5e533997082752fb082912cf35b2de08
夏健 <xiajian@boohee.com>
2019-11-13 22:12:02 +0800

    多次 load 导致的异常

--- lib/js_namespace_rails/action_view/helpers/asset_tag_helper.rb
+++ lib/js_namespace_rails/action_view/helpers/asset_tag_helper.rb
@@ -1,15 +1,17 @@
-
 module ActionView::Helpers::AssetTagHelper
 
-  alias_method :javascript_include_tag_without_controller, :javascript_include_tag
+  # load twice will raise exception 'stack level too deep'
+  unless defined?(:javascript_include_tag_without_controller)    
+    alias_method :javascript_include_tag_without_controller, :javascript_include_tag
 
-  def javascript_include_tag(*source)
+    def javascript_include_tag(*source)
 
-    if defined?(controller_path) && !@_included
-      @_included = true
-      concat javascript_tag(insert_hook_script, defer: 'defer')
+      if defined?(controller_path) && !@_included
+        @_included = true
+        concat javascript_tag(insert_hook_script, defer: 'defer')
+      end
+      javascript_include_tag_without_controller(*source)
     end
-    javascript_include_tag_without_controller(*source)
   end
 
 end
